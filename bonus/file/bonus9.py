8c4c22582707ae1839795209efb8e277e4c771a4
ankush.singal <andysingal@gmail.com>
2023-01-08 14:13:03 +0530

    First GUI code

---
+++ bonus/file/bonus9.py
@@ -0,0 +1,25 @@
+import PySimpleGUI as sg
+
+# label1 = sg.Text(text="Select files to compress")
+# input1 = sg.Input()
+# choose_button1 = sg.FileBrowse("Choose")
+#
+# label2 = sg.Text(text="Select destination folder")
+# input2 = sg.Input()
+# choose_button2 = sg.FolderBrowse("Choose")
+#
+# compress_button = sg.Button("Compress")
+# window = sg.Window('File Compressor',layout=[[label1,input1,choose_button1],[label2,input2,choose_button2],[compress_button]])
+# window.read()
+# window.close()
+#
+# label1 = sg.Text(text="Enter feet")
+# input1 = sg.Input()
+#
+# label2 = sg.Text(text="Enter inches")
+# input2 = sg.Input()
+#
+# compress_button = sg.Button("Convert")
+# window = sg.Window('Converter',layout=[[label1,input1],[label2,input2],[compress_button]])
+# window.read()
+# window.close()
